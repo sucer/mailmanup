6787c7d2df8246411bc87c977838cc9001cd93f7
Andres Sucerquia <andressucer@gmail.com>
2013-07-06 22:59:32 -0500

    nueva migracion

--- application/migrations/2013_03_23_160022_insersion_datos_iniciales.php
+++ application/migrations/2013_03_23_160022_insersion_datos_iniciales.php
@@ -9,29 +9,7 @@ class Insersion_Datos_Iniciales {
 	 */
 	public function up(){
 		//
-		DB::table('tipo_atributo')->insert( array(
-				array(
-				'tipo_atributo' => 'correo',
-	 			'validacion' => '[\w-\.]{3,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}',
-				),
-				array(
-				'tipo_atributo' => 'celular',
-	 			'validacion' => '[0-9]{10}',
-				),
-				array(
-				'tipo_atributo' => 'fecha',
-	 			'validacion' => '^[0-3]{1}[0-9]{1}\/[0-1]{1}[0-9]{1}\/[0-9]{4}$',
-				),
-				array(
-				'tipo_atributo' => 'numero',
-	 			'validacion' => '[0-9]+$',
-				),
-				array(
-				'tipo_atributo' => 'texto',
-	 			'validacion' => '.*',
-				),
-			)
-		);
+		
 	}
 
 	/**
---
+++ application/migrations/2013_07_07_035854_datos_iniciales.php
@@ -0,0 +1,48 @@
+<?php
+
+class Datos_Iniciales {
+
+	/**
+	 * Make changes to the database.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		DB::table('tipo_atributo')->insert( array(
+				array(
+				'tipo_atributo' => 'correo',
+	 			'validacion' => '[\w-\.]{3,}@([\w-]{2,}\.)*([\w-]{2,}\.)[\w-]{2,4}',
+				),
+				array(
+				'tipo_atributo' => 'celular',
+	 			'validacion' => '[0-9]{10}',
+				),
+				array(
+				'tipo_atributo' => 'fecha',
+	 			'validacion' => '^[0-3]{1}[0-9]{1}\/[0-1]{1}[0-9]{1}\/[0-9]{4}$',
+				),
+				array(
+				'tipo_atributo' => 'numero',
+	 			'validacion' => '[0-9]+$',
+				),
+				array(
+				'tipo_atributo' => 'texto',
+	 			'validacion' => '.*',
+				),
+			)
+		);
+	}
+
+	/**
+	 * Revert the changes to the database.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
