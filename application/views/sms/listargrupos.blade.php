ed69e715efc5a20b86a59ec68c6e387e201fce78
Andres Sucerquia <andressucer@gmail.com>
2013-08-09 22:53:58 -0500

    rap

--- application/views/sms/listargrupos.blade.php
+++ application/views/sms/listargrupos.blade.php
@@ -34,7 +34,7 @@
 	</h2>
 	<div class="row-fluid">
 		<div class="span10">
-			<div>{{__('grupos.mensaje-cantidad-grupos')}} <span>{{ $grupos->results->total }}</span></div>
+			<div>{{__('grupos.mensaje-cantidad-grupos')}} <span>{{ $grupos['total'] }}</span></div>
 		</div>
 	</div>
 
