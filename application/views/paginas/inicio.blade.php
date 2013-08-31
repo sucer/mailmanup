1210a2610280ea0719f91a5e512f4d62c85c443c
Andres Sucerquia <andressucer@gmail.com>
2013-08-31 15:44:05 -0500

    login with

--- application/views/paginas/inicio.blade.php
+++ application/views/paginas/inicio.blade.php
@@ -7,6 +7,11 @@
 	<h2>
 		{{__('inicio.eslogan')}}
 	</h2>
+
+	<div class="log-in"> 
+		<a href="/auth/twitter" class="btn-twitter">Entra con <strong>Twitter</strong></a>
+		<a href="/auth/facebook" class="btn-facebook">Entra con <strong>Facebook</strong></a>
+	</div>
 	
 	<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem mollitia odio ipsum unde iure molestiae sint architecto commodi! Quasi, deleniti a tenetur perspiciatis soluta ab eius quam officia tempore quisquam!</div>
 	<div>Consequatur, et, corporis, ullam dicta eveniet maxime magni nulla aspernatur velit labore unde delectus sint quia quibusdam dolorem laudantium alias ratione nemo quos perspiciatis nam nostrum molestiae dolorum dolores eligendi.</div>
